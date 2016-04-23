218bfd44766c7ab1c0ecacf58b5cab41c0cf7a4b
Bacor <Bas@vpn-stud-146-50-150-1.vpn.uva.nl>
2016-04-23 18:07:32 +0200

    draw-fst follows extension convention

--- utils/draw-fst.sh
+++ utils/draw-fst.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 
 ###
-# Draws the FST by its id and directory. For example:
-# ./draw-fst 4 ../data/fsts
-# writes a postscript file fst-4.ps to the ../data/fsts directory.
-# Directory is optional and default is the current one.
+# Draws an FST by its name. We assume the fst has extension .fst,
+# the isymbols are in the .isyms file and osymbols in .osyms. Exapmle:
+# ./draw-fst phrase-table-35
+# writes a postscript file phrase-table-35.ps to the current directory
 
-# Get & set arguments
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-i="$1"
-root="$2"
-if [ "$root" = "" ]; then
-	root="$DIR"
-	echo "No directory given, assuming $root"
-fi
+# Get name argument
+name="$1"
 
 # Files
-osyms="${root}/osyms-$i.txt"
-fst="${root}/fst-$i.fst"
-fst_binary="${root}/fst-$i.bin"
-fst_dot="${root}/fst-$i.dot"
-fst_ps="${root}/fst-$i.ps"
-fst_pdf="${root}/fst-$i.pdf"
+fst_binary="${name}.bin.tmp"
+fst_dot="${name}.dot.tmp"
+fst_ps="${name}.ps"
 
 # Go!
-fstcompile --osymbols="$osyms" "$fst" "$fst_binary" #>/dev/null
-fstdraw --osymbols="$osyms" "$fst_binary" "$fst_dot" >/dev/null
-dot -Tps "$fst_dot" >"$fst_ps"
+fstcompile --isymbols="${name}.isyms" --osymbols="${name}.osyms" "${name}.fst" "$fst_binary"
+fstdraw --isymbols="${name}.isyms" --osymbols="${name}.osyms" "$fst_binary" "$fst_dot"
+dot -Tps "$fst_dot" > "$fst_ps"
 # For PDF (doesn't work for Bas)
 # dot -Tpdf "$fst_dot" > "${root}/fst-$i.pdf"
-echo "Finished drawing FST: $fst_ps"
 
-# Clean up
+# Done
+echo "Finished drawing FST: ${name}.ps"
 rm "$fst_binary"
 rm "$fst_dot"
