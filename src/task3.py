3aea1c868d68953e821bee4a3e7dc4f65414cc01
maartjeth <maartjeterhoeve@gmail.com>
2016-04-29 16:59:00 +0200

    start find shortest path

--- src/task3.py
+++ src/task3.py
@@ -1,14 +1,27 @@
 import subprocess
 
-def task3(src_fst, trnsl_fst, out_dir, line_num):
+def task3(src_fst, trnsl_fst, out_dir, line_num, n=100):
 
-	outfile = "../data/composition-fsts/combined-test.fst"
+	outfst = "../data/composition-fsts/combined-test.fst" # this is for testing purposes now, need to change in final version
+	compose_fst(src_fst, trnsl_fst, outfst)
+
+
+	short_fst = "../data/short-path-fsts/short-test.fst"
+	find_n_best(str(n), outfst, short_fst)
+
+
+
+def compose_fst(src_fst, trnsl_fst, outfile):
 	# composition of the two fsts
 	call = "fstcompose " + src_fst + " " + trnsl_fst + " " + outfile
-	print "CALL: ", call
 	subprocess.call([call], shell=True)
-	
 
+def find_n_best(n, fst, short_fst):
+	#call = "fstshortestpath " + " " + n + " " + fst + " " + short_fst
+	call = "fstshortestpath " + fst + " " + short_fst
+	subprocess.call([call], shell=True)
+	# TODO: still include n
+	#fstshortestpath [--opts] a.fst out.fst
 
 if __name__ == '__main__':
 	src_fst = "../data/sorted-fsts/fst-sort-35.fst"
