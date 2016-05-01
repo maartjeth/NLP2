f2e0e42622af0d8542d828011df3dfbd1f07c8d2
bjmcornelissen@gmail.com <Bas@Bass-MBP.fritz.box>
2016-05-01 22:49:01 +0200

    Quick draw

---
+++ src/quick_draw.py
@@ -0,0 +1,12 @@
+from FST import *
+
+i = 35
+input_fst = FST("../data/1-inputs/input-%s" % i)
+phrase_table_fst = FST("../data/2-phrase-tables/phrase-table-%s" % i)
+translation = FST("../data/3-mono-translations/mono-translation-%s" % i)
+best_derivation = FST("../data/4-best-mono-derivations/mono-translation-%s.100best" % i)
+
+# input_fst.draw()
+# phrase_table_fst.draw()
+# translation.draw()
+best_derivation.draw()
